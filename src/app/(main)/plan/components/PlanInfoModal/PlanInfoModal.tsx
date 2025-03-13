c5e61adca4c4d758871812efece1db8759ebe073
JA_X <envijs93@gmail.com>
2025-03-14 07:33:48 +0900

    feat: Modal 관련 컴포넌트 폴더화

--- src/app/(main)/plan/components/PlanInfoModal/PlanInfoModal.tsx
+++ src/app/(main)/plan/components/PlanInfoModal/PlanInfoModal.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react';
-import { Button, VStack } from '@chakra-ui/react';
+import { Box, Button, VStack } from '@chakra-ui/react';
 import { DialogBody, DialogContent, DialogFooter, DialogHeader, DialogRoot, DialogTitle } from '@/components/ui/dialog';
 import usePlanStore from '@/store/usePlanInfoStore';
 import TimeSelector from './TimeSelector';
 import TransportPicker from './TransportPicker';
 import LocationPicker from './LocationPicker';
+import RouteSelector from './RouteSelector';
 import { PlanInfo } from '@/types/interface';
-// import generateSchedule from '@/utils/generateSchedule';
+import generateSchedule from '@/utils/generateSchedule';
+import { fetchDistanceMatrix } from '@/lib/api/distance';
+import calculateTravelTimes from '@/utils/calculateTravelTimes';
 
 function PlanInfoModal() {
   const [isOpen, setIsOpen] = useState<boolean>(true);
@@ -16,10 +19,8 @@ function PlanInfoModal() {
     transport: '',
     geocode: { lat: 0, lng: 0 },
     formattedAddress: '',
+    routeType: '',
   });
-
-  // const test = generateSchedule('10:00', '12:00');
-  // console.log(test);
 
   const { setPlanInfo: setGlobalPlanInfo } = usePlanStore();
   const handleUpdatePlanInfo = (updates: Partial<PlanInfo>) => {
@@ -27,21 +28,57 @@ function PlanInfoModal() {
   };
 
   // 저장 버튼 클릭 시 전역 상태 업데이트
-  const handleSave = () => {
+  const handleSave = async () => {
     setGlobalPlanInfo(planInfo);
     setIsOpen(false);
+
+    // const testparams = {
+    //   startTime: planInfo.startTime[0],
+    //   endTime: planInfo.endTime[0],
+    //   latitude: planInfo.geocode.lat,
+    //   longitude: planInfo.geocode.lng,
+    // };
+    // const testparams = {
+    //   startTime: planInfo.startTime[0],
+    //   endTime: planInfo.endTime[0],
+    //   latitude: planInfo.geocode.lat,
+    //   longitude: planInfo.geocode.lng,
+    // };
+
+    // const result = await generateSchedule(testparams);
+
+    // const finaltestparams = {
+    //   schedule: result,
+    //   // mode: planInfo.transport === '도보' ? 'walking' : 'driving',
+    //   mode: 'transit',
+    //   routeType: '왕복',
+    //   currentLocation: planInfo.geocode,
+    // };
+    // const finalResult = await calculateTravelTimes(finaltestparams);
+
+    // console.log(finalResult);
+
+    // const testparams = {
+    //   origin: planInfo.geocode,
+    //   destination: 'ChIJt9JrGUC5fDURm12ziVmivsk',
+    //   mode: 'driving',
+    // };
+
+    // const result = await fetchDistanceMatrix(testparams);
+    // console.log(result);
   };
 
   return (
     <DialogRoot open={isOpen}>
-      <DialogContent width="60%" height="80%" maxWidth="90vw" maxHeight="80vh" margin="auto">
-        <DialogHeader borderWidth={3}>
+      <DialogContent width="60%" height="80%" maxWidth="90vw" maxHeight="80vh" margin="auto" borderRadius={10}>
+        <DialogHeader>
           <DialogTitle>일정 추천을 위한 사전 질문</DialogTitle>
         </DialogHeader>
         <DialogBody borderWidth={3}>
           <VStack align="center" gap={4}>
             <TimeSelector startTime={planInfo.startTime} endTime={planInfo.endTime} onUpdate={handleUpdatePlanInfo} />
             <TransportPicker transport={planInfo.transport} onUpdate={handleUpdatePlanInfo} />
+            <RouteSelector routeType={planInfo.routeType} onUpdate={handleUpdatePlanInfo} />
             <LocationPicker formattedAddress={planInfo.formattedAddress} onUpdate={handleUpdatePlanInfo} />
           </VStack>
         </DialogBody>
