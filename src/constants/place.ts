2dd8b036f8c9eed5bae3d05886f76cef95030061
JA_X <envijs93@gmail.com>
2025-03-21 02:55:02 +0900

    feat: 장소 카테고리 리스트 상수화

---
+++ src/constants/place.ts
@@ -0,0 +1,13 @@
+export const PLACES_CATEGORY = {
+  관광지: 'tourist_attraction',
+  음식점: 'restaurant',
+  카페: 'cafe',
+  쇼핑몰: 'shopping_mall',
+  박물관: 'museum',
+  미술관: 'art_gallery',
+  공원: 'park',
+  영화관: 'movie_theater',
+  '바&펍': 'bar',
+} as const;
+
+export const DEFAULT_PLACES_CATEGORY = 'tourist_attraction';
