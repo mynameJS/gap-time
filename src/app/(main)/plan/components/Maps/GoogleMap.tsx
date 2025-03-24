5ec5821cd8978ad8912df68dad445b3a06d8f732
JA_X <envijs93@gmail.com>
2025-03-24 11:25:28 +0900

    feat: 현재위치 store 삭제 및 이전 planInfo 에서 입력된 current geocode 이용

--- src/app/(main)/plan/components/Maps/GoogleMap.tsx
+++ src/app/(main)/plan/components/Maps/GoogleMap.tsx
@@ -1,8 +1,8 @@
 'use client';
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useCallback } from 'react';
 import { GoogleMap, LoadScript, OverlayView } from '@react-google-maps/api';
 import { Box, Spinner, VStack, Text } from '@chakra-ui/react';
-import useCurrentLocationStore from '@/store/useCurrentLocationStore';
+import usePlanStore from '@/store/usePlanInfoStore';
 import useGeocodeListStore from '@/store/useGeocodeListStore';
 
 // 타입 정의
@@ -21,7 +21,7 @@ const mapOptions: google.maps.MapOptions = {
 function GoogleMaps() {
   // 추후 마커나 마우스무브, 세부페이지 이동 시에 사용예정
   const [map, setMap] = useState<google.maps.Map | null>(null); // 맵 객체 타입 정의
-  const { currentLocation, setCurrentLocation } = useCurrentLocationStore();
+  const { planInfo } = usePlanStore();
   const { geocodeList } = useGeocodeListStore();
 
   // onLoad 콜백
@@ -34,34 +34,20 @@ function GoogleMaps() {
     setMap(null); // 맵 객체 초기화
   }, []);
 
-  // ✅ 현재 위치 가져오기
-  useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        position => {
-          setCurrentLocation({
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          });
-        },
-        error => console.error(error),
-        { enableHighAccuracy: true }
-      );
-    }
-  }, []);
+  if (!planInfo) return null;
 
   return (
     <Box w="100%" h="100%" position="relative" borderRadius="lg" overflow="hidden" boxShadow="lg">
       <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY!} loadingElement={<Spinner />}>
         <GoogleMap
           mapContainerStyle={containerStyle}
-          center={currentLocation}
+          center={planInfo?.geocode}
           zoom={16}
           options={mapOptions}
           onLoad={onLoad}
           onUnmount={onUnmount}>
           {/* <Marker position={currentLocation} label={'현재 위치'} /> */}
-          <OverlayView position={currentLocation} mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}>
+          <OverlayView position={planInfo.geocode} mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}>
             <VStack align="center">
               <Text
                 fontSize="xs"
