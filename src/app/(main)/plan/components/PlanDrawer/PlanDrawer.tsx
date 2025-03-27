7de9427872068e64ac7007ae025aa112ac301f4e
JA_X <envijs93@gmail.com>
2025-03-28 07:04:48 +0900

    feat: 랜딩페이지에서 플랜페이지 이동 시 전달되는 쿼리스트링 값에 따라 Select 와 List 컴포넌트 분기 렌더링

--- src/app/(main)/plan/components/PlanDrawer/PlanDrawer.tsx
+++ src/app/(main)/plan/components/PlanDrawer/PlanDrawer.tsx
@@ -1,32 +1,30 @@
 'use client';
 
 import { useState } from 'react';
+import { useSearchParams } from 'next/navigation';
 import { Flex } from '@chakra-ui/react';
 import { CloseButton } from '@/components/ui/close-button';
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
-import usePlanStore from '@/store/usePlanInfoStore';
-import PlanInfoModal from '../PlanInfoModal/PlanInfoModal';
-import PlanSelector from './PlanSelector';
+import PlaceSelector from './PlaceSelector/PlaceSelector';
 import PlanList from './PlanList';
 
 function PlanDrawer() {
   const [isOpened, setIsOpened] = useState(true);
-  const { planInfo } = usePlanStore();
-
+  const searchParams = useSearchParams();
+  const mode = searchParams.get('mode');
   return (
     <>
       <Flex
         flexDirection="column"
         gap={1}
-        w="50rem"
+        w="60%"
         h="100%"
         position="relative"
         p={4}
         display={isOpened ? 'block' : 'none'} // ✅ 화면에서만 숨김
       >
-        {/* <PlanList /> */}
-        <PlanSelector />
-        {!planInfo && <PlanInfoModal />}
+        {mode === 'select' ? <PlaceSelector /> : <PlanList />}
+        {/* <PlaceSelector /> */}
         <CloseCustomButton onClick={() => setIsOpened(false)} />
       </Flex>
       {!isOpened && <OpenCustomButton onClick={() => setIsOpened(true)} />}
