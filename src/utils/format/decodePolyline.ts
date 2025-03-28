5de7af50e9cb54212b1eb85f03f65d26fbedfc5a
JA_X <envijs93@gmail.com>
2025-03-29 07:13:19 +0900

    feat: route api 호출 결과로 얻어지는 encode polyline 데이터 decoding 함수 구현

---
+++ src/utils/format/decodePolyline.ts
@@ -0,0 +1,45 @@
+// polyline decoding 함수
+
+function decodePolyline(encoded: string): google.maps.LatLngLiteral[] {
+  let index = 0;
+  const len = encoded.length;
+  let lat = 0;
+  let lng = 0;
+  const coordinates: google.maps.LatLngLiteral[] = [];
+
+  while (index < len) {
+    let result = 0;
+    let shift = 0;
+    let b: number;
+
+    do {
+      b = encoded.charCodeAt(index++) - 63;
+      result |= (b & 0x1f) << shift;
+      shift += 5;
+    } while (b >= 0x20);
+
+    const deltaLat = result & 1 ? ~(result >> 1) : result >> 1;
+    lat += deltaLat;
+
+    result = 0;
+    shift = 0;
+
+    do {
+      b = encoded.charCodeAt(index++) - 63;
+      result |= (b & 0x1f) << shift;
+      shift += 5;
+    } while (b >= 0x20);
+
+    const deltaLng = result & 1 ? ~(result >> 1) : result >> 1;
+    lng += deltaLng;
+
+    coordinates.push({
+      lat: lat / 1e5,
+      lng: lng / 1e5,
+    });
+  }
+
+  return coordinates;
+}
+
+export default decodePolyline;
