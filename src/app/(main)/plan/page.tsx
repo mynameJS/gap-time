643ae394ec6a7d2016e745cee2a4641723bb4f32
JA_X <envijs93@gmail.com>
2025-04-02 07:24:59 +0900

    feat: PlanRouteGuard 컴포넌트 plan 페이지 전역에 적용

--- src/app/(main)/plan/page.tsx
+++ src/app/(main)/plan/page.tsx
@@ -1,14 +1,18 @@
 import { Box, Flex } from '@chakra-ui/react';
 import PlanDrawer from './components/PlanDrawer/PlanDrawer';
 import GoogleMaps from './components/Maps/GoogleMap';
+import PlanRouteGuard from './components/PlanRouteGuard';
 
 export default async function Page() {
   return (
-    <Box height="100%" width="100%">
-      <Flex flexDir={{ base: 'column-reverse', md: 'row' }} height="100%" width="100%" position="relative">
-        <PlanDrawer />
-        <GoogleMaps />
-      </Flex>
-    </Box>
+    <>
+      <PlanRouteGuard />
+      <Box height="100%" width="100%">
+        <Flex flexDir={{ base: 'column-reverse', md: 'row' }} height="100%" width="100%" position="relative">
+          <PlanDrawer />
+          <GoogleMaps />
+        </Flex>
+      </Box>
+    </>
   );
 }
