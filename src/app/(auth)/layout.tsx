4af85eeee540b9037d88261376b768e088a930c4
JA_X <envijs93@gmail.com>
2025-04-04 15:21:15 +0900

    feat: auth 관련 페이지 폴더 생성 및 기본 route 설정

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from 'next';
+import { Box } from '@chakra-ui/react';
+
+export const metadata: Metadata = {
+  title: 'Time-Gap',
+  description: 'Time-Gap AuthPage',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Box width="100vw" height="100vh">
+        {children}
+      </Box>
+    </>
+  );
+}
