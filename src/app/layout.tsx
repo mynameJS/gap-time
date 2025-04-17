d58d922d389afabcafed79e0205d3c63ab61ef1f
JA_X <envijs93@gmail.com>
2025-04-17 23:19:46 +0900

    fix: meatadata 에 base url 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import QueryProvider from '@/components/providers/QueryProvider';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://www.gaptime.online'),
   title: 'Gap-Time',
   description: '틈새시간을 계획하는 새로운 방법',
   keywords: '틈새시간, 계획, 시간관리, 생산성, 효율성',
