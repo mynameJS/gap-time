68e00b79df4901f4f2f0fabc1c8706022d1b116e
JA_X <envijs93@gmail.com>
2025-04-17 20:12:44 +0900

    fix: 키워드 검색 시 리뷰수 기준이 아닌 googpe api 에서 기본제공하는 키워드 우선도 순위로 변경

--- src/app/(main)/plan/components/PlanDrawer/PlaceSelector/PlaceSearchPanel.tsx
+++ src/app/(main)/plan/components/PlanDrawer/PlaceSelector/PlaceSearchPanel.tsx
@@ -64,7 +64,6 @@ function PlaceSearchPanel({
       return fetchNearbyPlacesDetail({
         latitude: planInfo.geocode.lat,
         longitude: planInfo.geocode.lng,
-        sortBy: 'distance',
         keyword: searchTerm,
       });
     },
